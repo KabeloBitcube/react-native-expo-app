367bc39978891464af45e2b80d3a4ce7aff76009
KabeloBitcube <kabelo.intern@bitcube.tech>
2025-07-24 11:20:04 +0200

    Delete habits

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,16 +3,16 @@ import { useAuth } from "@/lib/auth_context";
 import { Habit } from "@/types/databases.type";
 import { router, useFocusEffect } from "expo-router";
 import { useCallback, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { Query } from "react-native-appwrite";
-import { Button, Divider, Text } from "react-native-paper";
+import { Button, Divider, IconButton, Text } from "react-native-paper";
 
 export default function Index() {
   const { signOut, user } = useAuth();
   const [habits, setHabits] = useState<Habit[]>();
 
-  useFocusEffect (
-    useCallback (() => {
+  useFocusEffect(
+    useCallback(() => {
       if (!user) {
         router.replace("/auth");
       } else {
@@ -35,6 +35,16 @@ export default function Index() {
     }
   }
 
+  const handleDeleteHabit = async (habitId: string) => {
+    try {
+      await database.deleteDocument(DATABASE_ID, HABITS_COLLECTION_ID, habitId);
+      console.log("Habit deleted:", habitId);
+      setHabits((prevHabits) => prevHabits?.filter(habit => habit.$id !== habitId));
+    } catch (error) {
+      console.error("Error deleting habit:", error);
+    }
+  }
+
   return (
     <View style={styles.view}>
       <Button icon="account-arrow-left" onPress={async () => {
@@ -48,12 +58,18 @@ export default function Index() {
         <Text variant="bodyLarge">No habits found. Start by creating one!</Text>
       ) : (habits?.map((habit) => (
         <View key={habit.$id}>
-          <Text variant="titleMedium">{habit.title}</Text>
-          <Text variant="bodyMedium">{habit.description}</Text>
-          <Text variant="bodySmall">{habit.frequency}</Text>
-          {/* <Text variant="bodySmall">Streak: {habit.streak_count}</Text> */}
-          {/* <Text variant="bodySmall">Last Completed: {habit.last_completed}</Text> */}
-          <Divider style={styles.divider}></Divider>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Text variant="titleMedium">{habit.title}</Text>
+            <Text variant="bodyMedium">{habit.description}</Text>
+            <Text variant="bodySmall">{habit.frequency}</Text>
+            {/* <Text variant="bodySmall">Streak: {habit.streak_count}</Text> */}
+            {/* <Text variant="bodySmall">Last Completed: {habit.last_completed}</Text> */}
+            <View style={{ flexDirection: "row" }}>
+              <IconButton size={20} icon="delete" onPress={() => handleDeleteHabit(habit.$id)}></IconButton>
+              <IconButton size={20} icon="square-edit-outline" onPress={() => handleDeleteHabit(habit.$id)}></IconButton>
+            </View>
+            <Divider style={styles.divider}></Divider>
+          </ScrollView>
         </View>
       ))
       )}
